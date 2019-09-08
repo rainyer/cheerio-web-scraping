34328054d4933bbe07327f398793446a1eb59979
Rainyer Santiago <rainyer@sonatalab.com>
2019-09-08 20:13:26 -0300

    initial project files

---
+++ index.js
@@ -0,0 +1,2 @@
+const getProduct = require('./product-scraping')
+// getProduct(https://www.americanas.com.br/produto/133718358/)
---
+++ product-scraping/index.js
@@ -0,0 +1,5 @@
+// Nosso desafio consiste na implementação de um módulo Node.js que permita receber o URL de um produto da americanas.com e retorne um JSON com informação deste produto.
+
+module.exports = function(url = null) {
+  if (!url) return console.log('Please provide a valid url parameter.')
+}
